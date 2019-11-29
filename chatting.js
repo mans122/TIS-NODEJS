9a2ac3832aab3fe2f95563041e21d33dd847db56
mans122 <gksqufdhkdwk2@gmail.com>
2019-11-29 14:59:49 +0900

    채팅

--- chatting.js
+++ chatting.js
@@ -2,6 +2,8 @@
 var http=require('http');
 var fs=require('fs');
 var socketio=require('socket.io');
+const hostname = '192.168.0.101';
+const port = 80;
 
 //웹서버 생성
 var server=http.createServer(function (request,response){
@@ -13,3 +15,14 @@ var server=http.createServer(function (request,response){
 }).listen(52273, function(){
     console.log('server running at http://127.0.0.1:52273');
 });
+
+//소켓버서 생성
+var io=socketio.listen(server);
+//connection은 내장 이벤트같음
+io.sockets.on('connection',function(socket){
+    //message는 사용자정의 이벤트
+    socket.on('message',function(data){
+        //클라이언트한테 message이벤트 발생시킴.
+        io.sockets.emit('message',data);
+    })
+})
